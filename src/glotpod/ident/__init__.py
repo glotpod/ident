5119473b0bb842d3e15ab7903a68880b1cba6a53
Te-jé Rodgers <tjd.rodgers@gmail.com>
2016-03-20 21:11:32 -0400

    feat config: add configuration primitives

--- src/glotpod/ident/__init__.py
+++ src/glotpod/ident/__init__.py
@@ -1,4 +1,11 @@
+from pathlib import Path
+
+from glotpod.config import Config
 from .__version__ import version as __version__
 
 
-__all__ = ['__version__']
+cfg = Config.from_env('GLOTPOD_IDENT_SETTINGS',
+                      str(Path(__file__).with_name('defaults.toml')))
+
+
+__all__ = ['__version__', 'cfg']
